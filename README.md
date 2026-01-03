3c8a7ed505b5d872b7fd33011d9aff802f5c7a2d
Timothy Lowe <65925429+t111mll@users.noreply.github.com>
2026-01-03 06:32:41 +0000

    Add env var example and note on Windows volume mounts

---
+++ README.md
@@ -0,0 +1,1 @@
+On Windows Git Bash, use pwd -W for volume mounts
--- hello.sh
+++ hello.sh
@@ -3,3 +3,5 @@ set -e
 
 echo "Hello from inside a Docker container!"
 echo "Current date: $(date)"
+
+echo "Environment: ${APP_ENV:-development}"
